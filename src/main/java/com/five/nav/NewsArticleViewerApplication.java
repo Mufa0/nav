855a1f450a88cf6ccec7bd9eabd9f9221db3b7e9
Mufa <mateostjepanovic@gmail.com>
2021-02-22 17:18:03 +0100

    Initial commit with project setup

---
+++ src/main/java/com/five/nav/NewsArticleViewerApplication.java
@@ -0,0 +1,13 @@
+package com.five.nav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsArticleViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsArticleViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/five/nav/NewsArticleViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.five.nav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsArticleViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
